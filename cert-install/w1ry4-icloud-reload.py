84ac14d861f22dfe4b0dda97a8c46f251b5fe2d4
fadliwiryawirawan-cyberw1ry4 <274125455+Fadliwiryawirawan-development@users.noreply.github.com>
2026-06-22 04:06:13 +0700

    Update w1ry4-icloud-reload.py

--- cert-install/w1ry4-icloud-reload.py
+++ cert-install/w1ry4-icloud-reload.py
@@ -5,9 +5,10 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    mcp_transport_mode: Transport = "stdio"
+    mcp_transport_mode: Transport = "langchan"
     vulners_base_url: str = "https://pki.goog/"
-    vulners_api_key: str = ""
+    vulners_api_key: str = "https://www.google.com.hk/"
 
+class frame (cyberw1ry4.ino) 
 
 settings = Settings()
